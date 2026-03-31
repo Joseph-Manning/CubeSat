6bba0e32a3c91b332d2e577e8bf346c6d8cfaeb2
Paladin-Greg <kieranorr45@Gmail.com>
2026-03-31 19:06:55 +0100

    Add files via upload

--- mode_1_open_loop.ino
+++ mode_1_open_loop.ino
@@ -1,4 +1,4 @@
-//Open loop code
+//Open loop code Mode 1
 //Author:Kieran Orr
 
 //Libraries
@@ -7,60 +7,71 @@
 #include <SD.h>
 //Gyro data reading
 #include <Wire.h>
+#include <Adafruit_VEML7700.h>
+#include <TCA9548A.h>
 #include <Adafruit_ICM20948.h>
+#include <Adafruit_ICM20X.h>
 #include <Adafruit_Sensor.h>
 
-// 
-double dt, last_time, now;
+//naming
+TCA9548A mux; //multiplexer
+Adafruit_ICM20948 icm; // 9 DOF sensor
+
 //pins
 const int pulse = 9;
 const int check_read = 3;
-//Define PD function ignore for open loop parameter estimation
-double pd(double error, double error_last, double dt) {
-  float Kp = 1;
-  float Kd = 1;
-  double proportional = Kp*error;
-  double derivative = Kd*(error-error_last)/dt;
-  double output = proportional+derivative;
-  return output;
-}
-void setup() {
-  double last_time = 0;
-  Wire.begin(); //for gyro
+const int chipSelect = 10;
 
+//ICU struc
+sensors_event_t accel;
+sensors_event_t gyro;
+sensors_event_t temp;
+//SD
+File dataFile;
+
+void setup() {
+  //for gyro
+  Wire.begin();
+  mux.begin();
+  mux.openChannel(3);
+  icm.begin_I2C();
+  icm.setAccelRange(ICM20948_ACCEL_RANGE_4_G); //Sets max acceleration rate measureable
+  icm.setGyroRange(ICM20948_GYRO_RANGE_500_DPS); // Sets max rotation rate measureable
+  //SD
+  SD.begin(chipSelect);
+  pinMode(check_read, OUTPUT);
+  digitalWrite(check_read, LOW);
+  dataFile = SD.open("datalog.txt", FILE_WRITE);
+  //pulse
   analogWrite(pulse, 255);
   delay(1000);
   analogWrite(pulse, 0);
 }
 
 void loop() {
-  double now = millis();
-  double dt = now - last_time;
-  double last_time = now;
+  //data collecting
+  mux.openChannel(3);
+  icm.getEvent(&accel, &gyro, &temp);
+  
   //data logging
   // make a string for assembling the data to log:
   String dataString = "";
 
   // read sensor and append to the string:
-  for (int analogPin = 0; analogPin < 1; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 0) {
-      dataString += ",";
-    }
-  }
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    float gyro_z = gyro.gyro.z;
+    dataString += String(gyro_z);
+  
 
   // if the file is available, write to it:
   if (dataFile) {
+    unsigned long t = millis();
     dataFile.println(dataString);
-    dataFile.close();
+    dataFile.println(",");
+    dataFile.println(t);
   }
   else{
    //as this is not connected to pc use led to check
    digitalWrite(check_read, HIGH);
   }
+  delay(50); //The delay between loops
 }
