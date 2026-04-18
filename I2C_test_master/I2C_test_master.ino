3ad860297cd2de5be3d85260e92186e5a55a93ad
Paladin-Greg <kieranorr45@Gmail.com>
2026-04-19 00:47:20 +0100

    I2C testing

---
+++ I2C_test_master/I2C_test_master.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+
+#define SLAVE_ADD 9 //define slave address
+
+#define ANSWERSIZE 5 //slave answer size
+
+void setup() {
+  Wire.begin(); //I2C com
+
+  //start serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(50);
+  Serial.println("write to slave");
+  
+  //write to slave
+  Wire.beginTransmission(SLAVE_ADD);
+  Wire.write(0);
+  Wire.endTransmission();
+
+  //recieve data
+  Serial.println("data from slave");
+  Wire.requestFrom(SLAVE_ADD, ANSWERSIZE);
+
+  String response = "";
+  while (Wire.available()) {
+    char b = Wire.read();
+    response += b;
+  }
+  Serial.println(response);
+}
