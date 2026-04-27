f4b8cf49937990a0edc087a2ddf8f47fc6f8e1be
Paladin-Greg <kieranorr45@Gmail.com>
2026-04-27 04:42:31 +0100

    removed bulk

--- Final Flash/B/B.ino
+++ Final Flash/B/B.ino
@@ -1,164 +1,117 @@
-/*Final script to flash to arduino A
-This is the slave on the I2C bus
-Author Kieran Orr
-Contributors
-Seb
-Finlay*/
+/* Final script to flash to Arduino A
+   Slave on the I2C bus
+   Author: Kieran Orr
+   Contributors: Seb, Finlay
+*/
 //======================================================================//
-//Libraries
-#include <Wire.h>  //included for I2C communication
-
-//Data logging
+// Libraries
+#include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
-//======================================================================//
-//Define address of slave Arduino
-#define SLAD 9
-//======================================================================//
-/*Pin map
-10 Chip select on SD shield
-11 MOSI on SD shield
-12 MISO on SD shield
-13 SCK on SD shield
-3.3V on SD shield
-GND spare GND port
 
-A4 Data pin (blue I2C connect)
-A5 Clock pin (yellow I2C connect)
-GND shared with master GND*/
 //======================================================================//
-//pins
-//Mode ID pin
+// I2C address
+#define SLAD 9
+
+//======================================================================//
+// Pins
 const int RP = 9;
-//Fan pins
 const int T4 = 3;
 const int H1 = 5;
 const int H2 = 6;
 
-//SD
 const int chipSelect = 10;
-//======================================================================//
-// FIFO BUFFER DEFINITIONS
-#define PACKET_SIZE 6
-#define BUFFER_SIZE 40   // large enough to absorb bursts
-
-volatile byte packetBuffer[BUFFER_SIZE][PACKET_SIZE];
-volatile int writeIndex = 0;
-volatile int readIndex = 0;
-volatile int packetCount = 0;
 
 //======================================================================//
-//define data stores
-//assemble on slave side
+// Data stores
 volatile byte array[7];
+volatile bool dataReady = false;
 
-//ready to read from receive event
-volatile bool dataReady;
+volatile bool gyroReady = false;
+volatile bool thetaReady = false;
+volatile bool errorReady = false;
+volatile bool motorReady = false;
 
-//mode 1 data check update
-volatile bool gyroReady;
-volatile bool thetaReady;
-volatile bool errorReady;
-volatile bool motorReady;
-
-//mode id
 unsigned long RP_val;
 
-//SD
-File dataFile;
-//======================================================================//
-//define global variables
-//data to save
 float gyro_z;
 float theta_sen;
 float error;
 float motor_pwm;
 
-int counter = 1;  // save every 20 cycles
+File dataFile;
+
 //======================================================================//
-//define function for data recieved
-// ISR: store each full packet into FIFO
+// I2C receive ISR
 void receiveEvent(int len) {
-  if (len != PACKET_SIZE) {
-    while (Wire.available()) Wire.read();
-    return;
-  }
-
-  // Copy packet into FIFO slot
-  for (int i = 0; i < PACKET_SIZE; i++) {
-    packetBuffer[writeIndex][i] = Wire.read();
-  }
-
-  // Advance write index
-  writeIndex = (writeIndex + 1) % BUFFER_SIZE;
-
-  // Handle overflow (drop oldest)
-  if (packetCount < BUFFER_SIZE) {
-    packetCount++;
+  if (len == 6) {
+    for (int i = 0; i < 6; i++) {
+      array[i] = Wire.read();
+    }
+    dataReady = true;
   } else {
-    readIndex = (readIndex + 1) % BUFFER_SIZE;
+    while (Wire.available()) Wire.read();
   }
 }
-//======================================================================//
 
+//======================================================================//
 void setup() {
-  //mode pin
   pinMode(RP, INPUT);
 
-  //prepare data bools
-  dataReady = 0;
-  gyroReady = 0;
-  thetaReady = 0;
-  errorReady = 0;
-  motorReady = 0;
+  // --- SD FIRST (NO INTERRUPTS ACTIVE) ---
+  SD.begin(chipSelect);
+  dataFile = SD.open("datalog.txt", FILE_WRITE);
+  dataFile.println("Time [ms], Theta [rad], Omega [rad/s], Error [rad], Command [PWM]");
 
-  //initialise I2C
+  // --- NOW enable I2C (SAFE) ---
   Wire.begin(SLAD);
   Wire.onReceive(receiveEvent);
-
-  //SD
-  SD.begin(chipSelect);
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-  dataFile.println("Time [ms], Theta [rad], Omega [rad/s], Error [rad], Command [PWM]");
 }
 
+//======================================================================//
 void loop() {
+
   RP_val = pulseIn(RP, HIGH, 30000);
+
   if (RP_val < 990) {
+
     if (dataReady) {
+      noInterrupts();   // protect array during decode
       dataReady = false;
+
       if (array[4] == 0 && array[5] == 0) {
         memcpy(&theta_sen, array, 4);
-        thetaReady = 1;
+        thetaReady = true;
       }
       if (array[4] == 1 && array[5] == 0) {
         memcpy(&gyro_z, array, 4);
-        gyroReady = 1;
+        gyroReady = true;
       }
       if (array[4] == 0 && array[5] == 1) {
         memcpy(&error, array, 4);
-        errorReady = 1;
+        errorReady = true;
       }
       if (array[4] == 1 && array[5] == 1) {
         memcpy(&motor_pwm, array, 4);
-        motorReady = 1;
+        motorReady = true;
       }
-    }
-    if (gyroReady == 1 && thetaReady == 1 && errorReady == 1 && motorReady == 1) {
-      //data logging
-      // make a string for assembling the data to log:
-      String dataString = "";
 
-      //write values to the string:
+      interrupts();
+    }
+
+    if (gyroReady && thetaReady && errorReady && motorReady) {
+
+      String dataString = "";
       unsigned long t = millis();
+
       dataString += t;
       dataString += ",";
+
       if (theta_sen == 10) {
-        dataString += "Searching";
+        dataString += "Searching,";
         dataString += String(gyro_z);
         dataString += ",";
-        dataString += "Unknown";
-        dataString += ",";
+        dataString += "Unknown,";
       } else {
         dataString += String(theta_sen);
         dataString += ",";
@@ -167,20 +120,17 @@ void loop() {
         dataString += String(error);
         dataString += ",";
       }
+
       dataString += String(motor_pwm);
       dataString += ",";
-      //open the file
-      //if (counter == 1) {File dataFile = SD.open("datalog.txt", FILE_WRITE);} // see if we can get away without declaring FIle everytime
-      File dataFile = SD.open("datalog.txt", FILE_WRITE);
-      // if the file is available, write to it:
-      if (dataFile) {
-        dataFile.println(dataString);
-      }
-      gyroReady = 0;
-      thetaReady = 0;
-      errorReady = 0;
-      motorReady = 0;
-    } else {
+
+      // --- SAFE SD WRITE ---
+      noInterrupts();
+      dataFile.println(dataString);
+      dataFile.flush();
+      interrupts();
+
+      gyroReady = thetaReady = errorReady = motorReady = false;
     }
   }
 }
